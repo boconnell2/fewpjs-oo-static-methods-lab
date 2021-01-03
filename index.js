4d7e9fe91b41dd741f374528da561f6a6fb56a47
boconnell2 <bmoconnell15@gmail.com>
2021-01-03 15:05:13 -0500

    commit tree 40e687f898cc1ba52eb4f8bd604c64e4e02fecef

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  //add static methods here 
+  static capitalize(string) {
+    let newString = string.charAt(0).toUpperCase() + string.slice(1)
+    return newString
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string){
+    let stringArray = string.split(' ')
+    let modifiedString = ''
+    let counter = 0
+    let dontCapitalize = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    stringArray.forEach((word) => {
+      if (counter = 0) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+      } else if (!dontCapitalize.includes(word)) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      modifiedString += (word + ' ')
+      counter++
+    })
+    return modifiedString.slice(0,-1)
+  }
 }
